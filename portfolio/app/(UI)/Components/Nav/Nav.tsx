5086259cfd847e32ab15ccb45a1c5bae0401c6ac
ilya-6370 <kotk6370@gmail.com>
2024-08-09 23:11:14 +0100

    Navbar

--- portfolio/app/(UI)/Components/Nav/Nav.tsx
+++ portfolio/app/(UI)/Components/Nav/Nav.tsx
@@ -1,19 +1,19 @@
-import NavLink from './NavLink';
+import {NavItem} from './NavLink';
 import { Upwork } from '../../Icons/Icons';
 import ToggleButton from './ToggleButton';
 
 // This is a server component
 export default function Nav() {
   return (
-    <nav className="flex justify-between px-2 sm:px-4 md:px-8 lg:px-20 xl:px-52 py-2 items-center bg-white">
+    <nav className="flex justify-between px-2 sm:px-4 md:px-8 lg:px-20 xl:px-52 py-2 items-center bg-white z-20">
       {/* Render the toggle button as a client component */}
       <ToggleButton />
-      <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+      <div className="bg-white h-full right-0 top-16 flex items-center justify-center fixed hidden md:bg-transparent md:static md:top-0 w-full md:block md:w-auto" id="navbar-default">
         <ul className="flex-col md:flex-row flex md:space-x-6 lg:space-x-16 items-center">
-          <li><NavLink href=".">Intro</NavLink></li>
-          <li><NavLink href="#skills">Skills</NavLink></li>
-          <li><NavLink href="/portfolio/">Projects</NavLink></li>
-          <li><NavLink href="#contact">Contact me</NavLink></li>
+          <NavItem href=".">Intro</NavItem>
+          <NavItem href="#skills">Skills</NavItem>
+          <NavItem href="/portfolio/">Projects</NavItem>
+          <NavItem href="#contact">Contact me</NavItem>
         </ul>
       </div>
       <div>
--- portfolio/app/(UI)/Components/Nav/NavLink.tsx
+++ portfolio/app/(UI)/Components/Nav/NavLink.tsx
@@ -1,8 +1,13 @@
 import Link from 'next/link'
 // import React from 'react'
-
+export function NavItem({children, ...p}) {
+    return (
+       <li className='py-4'><NavLink {...p}> {children}</NavLink></li>
+    )
+  }
+  
 export default function NavLink({children, ...p}) {
   return (
-    <Link {...p} className='NavLink text-2xl'>{children}</Link>
+    <Link {...p} className='NavLink text-2xl transition-colors'>{children}</Link>
   )
 }
